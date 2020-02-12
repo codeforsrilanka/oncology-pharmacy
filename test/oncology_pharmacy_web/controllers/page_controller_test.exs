c7e4c60b4047f7fb98ba90ffa23056548fec4686
Oshan Wisumperuma <warunaoshan@gmail.com>
2020-02-12 08:17:07 +0530

    skip test

--- test/oncology_pharmacy_web/controllers/page_controller_test.exs
+++ test/oncology_pharmacy_web/controllers/page_controller_test.exs
@@ -1,6 +1,7 @@
 defmodule OncologyPharmacyWeb.PageControllerTest do
   use OncologyPharmacyWeb.ConnCase
 
+  @tag :skip
   test "GET /", %{conn: conn} do
     conn = get(conn, "/")
     assert html_response(conn, 200) =~ "Welcome to Phoenix!"
